20598a5b367ac67a0ac20a269c64d8f45cfcac2d
noorasif05 <noorasif.noor05@gmail.com>
2025-08-25 13:47:00 +0500

    added updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
 from tkinter import *
 from tkinter import messagebox
 import random
@@ -9,8 +7,14 @@ import pyperclip
 import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project
+
 def password_generator():
+    """
+     Generates a random password consisting of letters, numbers, and symbols.
+     Inserts the generated password into the password entry field
+     and copies it to the clipboard.
+     """
+
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -31,40 +35,14 @@ def password_generator():
     pyperclip.copy(password)
 
 
-
-window = Tk()
-canvas = Canvas(window, width=200, height=200)
-window.config(padx=20, pady=20)
-photoimage = PhotoImage(file='logo.png')
-canvas.create_image((100,100),image = photoimage)
-canvas.grid(row=0, column=1)
-
-
-
-
-
-#label
-website_label = Label(window, text="Website:")
-website_label.grid(row = 1,column = 0)
-email_label = Label(window, text="Email/Username:")
-email_label.grid(row = 2,column = 0)
-password_label = Label(window, text="password:")
-password_label.grid(row = 3,column = 0)
-
-#Entries
-website_entry = Entry(width = 21)
-website_entry.grid(row = 1,column = 1)
-website_entry.focus()
-email_entry = Entry(width = 35)
-email_entry.grid(row = 2,column = 1,columnspan = 2)
-email_entry.insert(0,"ahaankhan.ahaan@gmail.com")
-password_entry = Entry(width = 21)
-password_entry.grid(row = 3,column = 1)
-
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    """runs when add button is clicked
-        ,save data to file
-        ,removes the current data after saving """
+    """
+     Saves the website, email, and password into a JSON file.
+     - If file doesn't exist, creates a new one.
+     - Updates existing file with new credentials.
+     - Displays error if website or password field is empty.
+     """
     website = website_entry.get()
     password= password_entry.get()
     email = email_entry.get()
@@ -85,7 +63,16 @@ def save():
         finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
 def find_password():
+    """
+        Searches the JSON file for credentials of a given website.
+        Displays saved email and password in a popup.
+        """
     website = website_entry.get()
     try:
         with open("data.json","r") as file:
@@ -102,6 +89,39 @@ def find_password():
        else:
            messagebox.showerror(title="Error", message="No data found for this website")
 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+canvas = Canvas(window, width=200, height=200)
+window.config(padx=20, pady=20)
+photoimage = PhotoImage(file='logo.png')
+canvas.create_image((100,100),image = photoimage)
+canvas.grid(row=0, column=1)
+
+#labels
+website_label = Label(window, text="Website:")
+website_label.grid(row = 1,column = 0)
+
+email_label = Label(window, text="Email/Username:")
+email_label.grid(row = 2,column = 0)
+
+password_label = Label(window, text="password:")
+password_label.grid(row = 3,column = 0)
+
+#Entries
+website_entry = Entry(width = 21)
+website_entry.grid(row = 1,column = 1)
+
+website_entry.focus()
+email_entry = Entry(width = 35)
+
+email_entry.grid(row = 2,column = 1,columnspan = 2)
+email_entry.insert(0,"ahaankhan.ahaan@gmail.com")
+
+password_entry = Entry(width = 21)
+password_entry.grid(row = 3,column = 1)
 
 
 #buttons
